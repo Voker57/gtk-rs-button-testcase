46f4a1d5e211fc4894f54bbe6ff623f707bf959e
Iaroslav Gridin <voker57@gmail.com>
2020-10-23 15:14:59 +0300

    testcase

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use gtk::prelude::*;
+use gio::prelude::*;
+
+fn main() {
+	let application = gtk::Application::new(None, Default::default()).unwrap();
+
+
+	application.connect_activate(move |app| {
+		let window = gtk::ApplicationWindow::new(app);
+		window.set_title("testcase");
+				
+		window.connect_button_press_event(move |window, ev| {
+			println!("{:?}", ev.get_event_type());
+			gtk::Inhibit(false)
+		});
+		window.show_all();
+	});
+
+	application.run(&[]);
+} 
